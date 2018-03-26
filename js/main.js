4bae21e3aec4efb1d7c692b36ad9b0832d0452d1
Alana Kim <alanakim12@gmail.com>
2018-03-26 17:34:40 -0400

    still broken - refactoring IP
    trying to utilize underscore js functionalities and also now understanding more about structuring j3 elements.

--- js/main.js
+++ js/main.js
@@ -1,75 +1,169 @@
+var tripIndex; // define trip number 1~11
+var indexFields = ["trip", "olive"]
+, timeField = ["ftime"]
+, dataFields = ["trip", "olive", "ftime", "dust", "light", "tempF", "GINI_IND"]
+, locationFields = ["FIPS", ];
 
-    var tripIndex; // define trip number 1~11
+var selectFields = _.union(indexFields, timeField, dataFields);
 
-    // DEFINE TRIP DATA 
+// DEFINE TRIP DATA 
 
-    var thisData = []; 
-    var queryData = function (dataset, tripNum) {
-        thisData = _.filter(dataset.features, function (feature) {
-            return (feature.properties["trip"] === tripNum) 
-            && (feature.properties["olive"] > 5)
-        })
-    }; //works 
+var thisData = _.chain(sdata.features)
+            .filter(function (feature) {
+                return (feature.properties["trip"] === tripIndex) && (feature.properties["olive"] > 5)
+                })
+            .map(function (feature) {
+                return _.pick(feature.properties, _.map(selectFields, function (key) {return key}))
+                })
+            .value(); 
+            // needs to work more on this 
+            
+
+                .value()
+var queryData = function(dataset, tripNum) {
+    thisData = _.filter(dataset.features, function(feature) {
+        return (feature.properties["trip"] === tripNum) &&
+            (feature.properties["olive"] > 5)
+    })
+}; //works 
+
+//_.map(sdata.features, function (feature) {return _.pick(feature.properties, _.map(selectData, function (key) {return key}))})
+
+
+
+
+//canvas size variables 
+var graphWidth = 600;
+var graphHeight = 130;
+var graphMargin = { top: 40, right: 20, bottom: 30, left: 100 };
+
+// x and y axis setup
+var x = d3.scaleTime().range([0, graphWidth]);
+var y = d3.scaleLinear().range([graphHeight, 0]);
+
+// setting map object extent 
+var mapWidth = 800,
+    mapHeight = 650,
+    mapMargin = { top: 20, right: 10, bottom: 30, left: 20 }
+
+// map projection parameters
+// these were achieved by just adjusting decimal units multiple times
+// and refreshing the html page. should figure out automatic centering. 
+var center = [2.5725, 39.957049],
+    offset = [mapWidth / 2, mapHeight / 2],
+    scale = 700000,
+    PennSouthProjection = d3.geoConicConformal().scale(scale)
+    .parallels([39 + 56 / 60, 40 + 58 / 60])
+    .rotate([77 + 45 / 60, 0])
+    .center(center)
+    .translate(offset);
 
     // Data will change depending on the queryData() result, but the canvas sizes and svgs won't change.. 
     // let's define them first separately. 
 
-    var mapCanvas = function () {
 
-        // setting map object extent 
-        var mapWidth = 800,
-        mapHeight = 650,
-        mapMargin = { top: 20, right: 10, bottom: 30, left: 20}
 
-        // create map svg 
-        mapsvg = d3.select("#map")
-        .append("svg")
-        .attr("width", mapWidth + mapMargin.left + mapMaring.right)
-        .attr("height", mapHeight + mapMargin.top + mapMargin.bottom)
-        .attr("transform", "translate(" + mapMargin.left + "," + mapMargin.top + ")"); 
 
-        //?? Append empty placeholder g element to the SVG
-        // g will contain geometry elements
-        var g = mapsvg.append("g");
+    var displayGraphs = function () {
 
-        // map projection parameters
-        // these were achieved by just adjusting decimal units multiple times
-        // and refreshing the html page. should figure out automatic centering. 
-        var center = [2.5725, 39.957049],
-        offset = [mapWidth / 2, mapHeight /2],
-        scale = 700000,
-        PennSouthProjection = d3.geoConicConformal().scale(scale)
-                                                    .parallels([39 + 56 / 60, 40 + 58 / 60])
-                                                    .rotate([77+45/60, 0])
-                                                    .center(center)
-                                                    .translate(offset); 
+            // graph the all graph divs defined by classname "graphs"
+            var graphSeries = document.getElementsByClassName("graphs"); 
 
-        // Create GeoPath function that uses built-in D3 functionality to turn 
-        // lat/long coordinates into screen coordinates 
-        var geoPath = d3.geoPath()
-                        .projection(PennSouthProjection); 
+            // data prep: time variable + y variables
+            _.chain(thisData) 
+            .each(function (sensObj) {return ((sensObj.properties["ftime"]) ? 
+                sensObj.properties["ftime"] : new Date(sensObj.properties["unixt"] * 1000 + 18000000))}
+            .each(function (sensObj) {return (_.contains([selectData], _.keys(sensObj.properties))) ?
+                sensObj.properties[]
+                sensObj.properties["ftime"] : 
 
-    },
+                sensObj.properties_.each(selectData, function (propName) {sensObj.properties[propName] = +sensObj.properties[propName];});})
 
-    graphCanvas = function (chartID) {
-        //Width and height
-            var graphWidth = 600;
-            var graphHeight = 130;
+return _.map(thisData, function(sensObj) {return _.pick(sensObj.properties, "ftime", yData)});
 
-            var graphMargin = { top: 40, right: 20, bottom: 30, left: 100 };
-            var whichChart = document.getElementById(chartID).value;
-            console.log(whichChart);
-            var chart = d3.select(whichChart).append("svg")
+            _.each(thisData, function (sensObj) {
+                
+                });
+
+            // up to here works 
+
+            _.each(graphSeries, function (graphIndexEls) { 
+                _.chain(graphIndexEls)
+                .setupCanvas(graphIndexEl)
+                .createVisualization()
+                .renderGraph();});
+
+            // for each graph div looped through by the graphIndex,
+
+
+                //_.indexOf([array], value)
+
+            var mapGraphData; 
+            _.each(selectData, createVisualization); 
+
+
+
+            var setupCanvas(graphIndexEl) {
+                
+                var chart = d3.select(graphIndexEl).append("svg")
                 .attr("width", graphWidth + graphMargin.left + graphMargin.right)
                 .attr("height", graphHeight + graphMargin.top + graphMargin.bottom)
                 .append("g")
                 .attr("transform",
                     "translate(" + graphMargin.left + "," + graphMargin.top + ")");
 
+                chart.append("text")
+                    .attr("x", graphWidth - 70)
+                    .attr("y", 20)
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "16px")
+                    .style("fill", "teal")
+                    .style("text-decoration", "underline")
+                    .text("Title");
+            };
 
-            var x = d3.scaleTime().range([0, width]);
-            var y = d3.scaleLinear().range([height, 0]);
+         var prepData = function (thisGraph) {
+
+            var thisIndex = _.indexOf(graphSeries, thisGraph);
+            var yData = selectData[thisIndex]; 
+
+            var yMin = d3.min(_.map(thisData, function (sensObj) {return sensObj.properties[yData];})),
+             yMax = d3.max(_.map(thisData, function (sensObj) {return sensObj.properties[yData];}));
+
+            //console.log(yMin, yMax);
+
+            x.domain(d3.extent(thisData, function (sensObj) { return sensObj.properties["fttime"]; }));
+            y.domain([0.8*yMin, 1.2*yMax]); 
+
+            graphLine = d3.line()
+            .x(function(d) {return x(d["ftime"]);})
+            .y(function(d) {return y(d[yData]);}); 
+
+        }; //works
+
+
+
+            var renderGraph () {
+                                chart.append("path")
+                .attr("class", "line")
+                .attr("d", valueline(linedata));
+
+
+                chart.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+            chart.append("g")
+                .call(d3.axisLeft(y));
+            }
+
+
     };
+
+
+
+
+
 
 
 
@@ -89,6 +183,27 @@
 
 
 
+    var mapCanvas = function () {
+
+
+
+        // create map svg 
+        mapsvg = d3.select("#map")
+        .append("svg")
+        .attr("width", mapWidth + mapMargin.left + mapMaring.right)
+        .attr("height", mapHeight + mapMargin.top + mapMargin.bottom)
+        .attr("transform", "translate(" + mapMargin.left + "," + mapMargin.top + ")"); 
+
+        //?? Append empty placeholder g element to the SVG
+        // g will contain geometry elements
+        var g = mapsvg.append("g");
+
+        // Create GeoPath function that uses built-in D3 functionality to turn 
+        // lat/long coordinates into screen coordinates 
+        var geoPath = d3.geoPath()
+                        .projection(PennSouthProjection); 
+
+    }; 
 
     function displayData(tripnumber) {
 
@@ -125,33 +240,8 @@
 
 
 
-
-        ///// GRAPH 1
-
-        var renderGraph1 = function(dataset, tripN) {
-
-            var data = _.filter(sdata.features, function(dataset) { return (dataset.properties["trip"] === tripN) && (dataset.properties["olive"] != 1) });
-            console.log(data);
-
-            data.forEach(function(d) {
-                d.properties["ftime"] = +d.properties["ftime"];
-                d.properties["dust"] = +d.properties["dust"];
-                //console.log(d.properties["dust"]);
-            });
-
-
-            createVisualization1(data);
-        };
-
-        console.log(sdata.features[0]);
-        renderGraph1(sdata, tripN, "dust");
-        //console.log("yes");
-
-
         function createVisualization1(data) {
-
-
-            data.forEach(function(d) { d.properties["ftime"] = new Date(d.properties["unixt"] * 1000 + 18000000); });
+  
 
             x.domain(d3.extent(data, function(d) { return d.properties["ftime"]; }));
             y.domain([0, d3.max(data, function(d) { return d.properties["dust"]; })]);
