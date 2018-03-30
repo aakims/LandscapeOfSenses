712836b7672ae14c7b4722da97b5082951a014cd
aakims <alanakim12@gmail.com>
2018-03-30 17:09:36 -0400

    fully refactored & functional
    this marks the end of the code-cleanup branch work.
    
    Achieved: 1) buttons work properly; every time new trip button is clicked, the entire canvas clears then draws everything anew; 2) Four of the graphs are rendered using a series of functions (no repetition in d3 functions); 3) map also renders properly. Not much work has been done on that front

--- js/main.js
+++ js/main.js
@@ -10,31 +10,25 @@ var graphTitles = ["Air Quality", "Light Level", "Temperature", "Gini Index"];
 var graphVars = ["chart1", "chart2", "chart3", "chart4"];
 var chart1, chart2, chart3, chart4;
 var thisData = [];
-var lineData; 
+var lineData;
 // DEFINE TRIP DATA 
 
-var defineData = function (tripIndex) {
+var defineData = function(tripIndex) {
     thisData = _.chain(sdata.features)
-    .filter(function(feature) {
-        //console.log(sdata.features[1])
-        return ((feature.properties["trip"] === tripIndex) && (feature.properties["olive"] > 5))
-    })
-    .map(function(feature) {
-        feature.properties = _.pick(feature.properties, selectFields);
-        console.log(feature.properties);
-        console.log(feature);
-        return feature;
-    })
-    .map(function(feature) {
-            feature.properties["ftime"] =
-                (((feature.properties["unixt"]) > 0) ?
-                    new Date(feature.properties["unixt"] * 1000 + 18000000) : console.log("no time stamp"));
+        .filter(function(feature) {
+            //console.log(sdata.features[1])
+            return ((feature.properties["trip"] === tripIndex) && (feature.properties["olive"] > 5))
+        })
+        .map(function(feature) {
+            feature.properties = _.pick(feature.properties, selectFields);
+            console.log(feature.properties);
+            console.log(feature);
             return feature;
         })
-    .value();
-        
+        .value();
+
     console.log(thisData[1]);
-    return thisData; 
+    return thisData;
 
 };
 
@@ -61,7 +55,7 @@ var center = [2.5725, 39.957049],
     .center(center)
     .translate(offset);
 
-var graphSeries, graphItems, thisIndex; 
+var graphSeries, graphItems, thisIndex;
 
 
 var setupCanvas = function(thisItem) {
@@ -84,65 +78,186 @@ var setupCanvas = function(thisItem) {
 
 };
 
+var parseTime = d3.timeParse("%x");
+
 var prepData = function(yDataIndex) {
 
     //var thisIndex = _.indexOf(graphItems, graphItem);
+    lineData = JSON.parse(JSON.stringify(thisData));
     var yData = displayFields[yDataIndex];
-    console.log(yData); 
+
+    console.log(yData);
     //console.log(thisData[1]);
 
     var yMin = d3.min(lineData, function(sensObj) { return sensObj.properties[yData]; }),
         yMax = d3.max(lineData, function(sensObj) { return sensObj.properties[yData]; });
 
-    // console.log(yMin, yMax);
+    _.map(lineData, function(feature) {
+        feature.properties["ftime"] = new Date(feature.properties["unixt"] * 1000 + 18000000);
+        return feature;
+    })
+    var xMin = d3.min(lineData, function(sensObj) { return sensObj.properties["ftime"] });
+    var xMax = d3.max(lineData, function(sensObj) {
+        var formatted = new Date(sensObj.properties["ftime"]);
+        return formatted
+    });
+
+    //var xScale = d3.time.scale()
+    //.domain([xMin, xMax])
+    //.rnage
+
+    console.log(xMin, xMax);
+    console.log(yMin, yMax);
     //https://www.quora.com/How-do-I-use-string-content-as-variable-name-in-JS
 
-    console.log(lineData[0]); 
+    console.log(lineData[0]);
     console.log(thisData[0]);
 
-    x.domain(d3.extent(lineData, function(sensObj) { return sensObj.properties["fttime"]; }));
-    y.domain([0.75 * yMin, 1.25 * yMax]);
+    var xRange = xMax - xMin,
+    yRange = yMax - yMin; 
+    console.log(xRange, yRange);
+    x.domain([xMin - (xRange * 0.01), xMax]);
+    y.domain([yMin - (yRange * 0.1), yMax]);
+    //y.domain([yMin - (yRange * 0.05), yMax + (yRange * 0.05)]);
+
+    // set domain to be extent +- 5%
+//x.domain([xExtent[0] - (xRange * .05), xExtent[1] + (xRange * .05)]);
+//y.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);
 
     graphLine = d3.line()
-        .x(function(d) { return x(d["ftime"]); })
-        .y(function(d) { return y(d[yData]); });
+        .x(function(d) { console.log(d.properties); return x(d.properties["ftime"]) })
+        .y(function(d) { return y(d.properties[yData]); });
 
     lineData = _.map(lineData, function(sensObj) {
         //console.log("in lineData"); 
         sensObj.properties = _.pick(sensObj.properties, "ftime", yData);
-        return sensObj; 
+        sensObj.properties["ftime"] = +sensObj.properties["ftime"];
+        sensObj.properties[yData] = +sensObj.properties[yData];
+        return sensObj;
     });
     console.log(lineData);
 };
 
+var renderGraph = function() {
+
+    //console.log("Here");
+
+    var axisY = d3.axisLeft(y); 
+
+    axisY.ticks(5); 
+
+    chart.append("g")
+        .attr("transform", "translate(0," + graphHeight + ")")
+        .call(d3.axisBottom(x));
+
+    chart.append("g")
+        .call(axisY);
+
+    console.log(lineData);
+    chart.append("path")
+        .attr("class", "line")
+        .attr("d", graphLine(lineData));
+};
 
 
 var displayGraphs = function(tripIndex) {
-        //console.log(thisData);
-        //console.log(sdata.features);
+    //console.log(thisData);
+    //console.log(sdata.features);
+    clearCanvas();
+    console.log(tripIndex);
+    defineData(tripIndex);
+    //console.log("definedData example:" + thisData[10]) ;
+    // graph the all graph divs defined by classname "graphs"
+    graphSeries = document.getElementsByClassName("graphs");
+    graphItems = _.map(graphSeries, function(graphItem) { return graphItem.id });
+    console.log(graphItems);
+    //console.log(thisData[10]);
+    //prepData();
+    displayMap();
 
-        console.log(tripIndex);
-        defineData(tripIndex);
-        //console.log("definedData example:" + thisData[10]) ;
-        // graph the all graph divs defined by classname "graphs"
-        graphSeries = document.getElementsByClassName("graphs");
-        graphItems = _.map(graphSeries, function (graphItem) {return graphItem.id});
-        console.log(graphItems); 
-        //console.log(thisData[10]);
-        //prepData();
+
+    _.each(graphItems, function(graphItem) {
+
+        //(graphItem === "chart1") :
+        //defineData(tripIndex); 
+        console.log(thisData);
+        thisIndex = _.indexOf(graphItems, graphItem);
+        console.log(thisIndex);
+        var thisGraph = graphItem;
+        var chart, graphLine;
+        setupCanvas(graphItem);
+        prepData(thisIndex);
+        renderGraph();
+
+    });
+};
 
 
-        _.each(graphItems, function (graphItem) {
+var displayMap = function () {
 
-            //(graphItem === "chart1") :
-            //defineData(tripIndex); 
-             lineData = thisData; 
-             console.log(thisData); 
-             thisIndex = _.indexOf(graphItems, graphItem); 
-             console.log(thisIndex);
-             var thisGraph = graphItem;
-             var chart, graphLine;  
-             setupCanvas(graphItem); 
-             prepData(thisIndex);
+var mapsvg = d3.select("#map")
+        .append("svg")
+        .attr("width", mapWidth + mapMargin.left + mapMargin.right)
+        .attr("height", mapHeight + mapMargin.top + mapMargin.bottom)
+        .attr("transform",
+            "translate(" + mapMargin.left + "," + mapMargin.top + ")");
+
+        var geoPath = d3.geoPath()
+        .projection(PennSouthProjection);
+
+        var g = mapsvg.append("g");
+
+        g.selectAll("path")
+        .data(philly_neighborhoods.features)
+        .enter()
+        .append("path")
+        .attr("fill", "#ccc")
+        .attr("stroke", "#333")
+        .attr("d", geoPath);
+
+    var mapData = JSON.parse(JSON.stringify(thisData));
+    //console.log(mapData[0]);
+
+    var bubbles = mapsvg.append("g");
+
+    bubbles.selectAll("path")
+        .data(mapData)
+        .enter()
+        .append("path")
+        .attr("fill", "#900")
+        .attr("stroke", "#999")
+        .attr("d", geoPath)
+        .attr("class", "sensingpts")
+        .on("mouseover", function(d) {
+            d3.select("#dateval").text(d.properties["date"])
+            d3.select("#dustval").text(d.properties["dust"])
+            d3.select("#lightval").text(d.properties["light"])
+            d3.select("#tempval").text(d.properties["tempF"])
+            d3.select("#ginival").text(d.properties["GINI_IND"])
+            d3.select(this).attr("class", "sensingpts hover");
+        })
+        .on("mouseout", function(d) {
+            d3.select("#dateval").text("")
+            d3.select("#dustval").text("")
+            d3.select("#lightval").text("")
+            d3.select("#tempval").text("")
+            d3.select("#ginival").text("")
+            d3.select(this).attr("class", "sensingpts");
         });
-    };
+
+    //var datalocator = d3.line()
+};
+
+var clearCanvas = function() {
+
+    // ("#" + thisItem).append("svg")
+    _.each(graphItems, function(thisItem) {
+        $("#" + thisItem).empty();
+    });
+    $("#map").empty();
+
+    //var items = d3.select('svg').selectAll('.item').data(newData);
+
+    // Remove old elements:
+    //items.exit().remove();
+};
